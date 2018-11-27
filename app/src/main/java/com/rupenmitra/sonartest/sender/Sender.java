2a2e8e27831bbc33d646f9a3ebc459d0ddf3ab51
srander <shubham.rander23@gmail.com>
2018-11-26 22:47:06 -0500

    Optimizing the sine wave generation

--- app/src/main/java/com/rupenmitra/sonartest/sender/Sender.java
+++ app/src/main/java/com/rupenmitra/sonartest/sender/Sender.java
@@ -7,24 +7,26 @@ import android.os.Handler;
 
 public class Sender {
 
-    private static final int DURATION = 60; // seconds
-    private static final int SAMPLE_RATE = 44100;
+    private static final double FINAL_AMPLITUDE = 0.8 * 32767; // 32767 is highest positive integer for signed integer
+    private static final double FREQ_OF_TONE = 18600;
+    private static final int DURATION = 60;
+    private static final int SAMPLE_RATE = 44100; // This means one second has 44100 number of samples
     private static final int NUM_SAMPLES = DURATION * SAMPLE_RATE;
 
-    private static final double FREQ_OF_TONE = 300; // hz
-    private static final Handler HANDLER = new Handler();
+    private static final double TICKS_PER_SECOND = SAMPLE_RATE / FREQ_OF_TONE;
+    private static final double MULTIPLIER = 2 * Math.PI / TICKS_PER_SECOND;
 
+    private static final Handler HANDLER = new Handler();
 
     public void generateSound() {
         // Use a new tread as this can take a while
         final Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                final byte[] frequencyArray = genTone();
                 HANDLER.post(new Runnable() {
                     @Override
                     public void run() {
-                        playSound(frequencyArray);
+                        playSound(genTone());
                     }
                 });
             }
@@ -33,20 +35,19 @@ public class Sender {
     }
 
     private byte[] genTone() {
-        final double sample[] = new double[NUM_SAMPLES];
+        final short sample[] = new short[NUM_SAMPLES];
         final byte generatedSnd[] = new byte[2 * NUM_SAMPLES];
 
         // fill out the array
         for (int i = 0; i < NUM_SAMPLES; ++i) {
-            sample[i] = Math.sin(2 * Math.PI * i / (SAMPLE_RATE / FREQ_OF_TONE));
+            // amp * Math.sin(2*pi*i*(frequencyOfTone/SampleRate))
+            // multiplier is 2*pi*(freq/sample_rate)
+            sample[i] = (short) (FINAL_AMPLITUDE * Math.sin(MULTIPLIER * i));
         }
 
-        // convert to 16 bit pcm sound array
-        // assumes the sample buffer is normalised.
+        // convert to 16 bit pcm sound array, assumes the sample buffer is normalised.
         int idx = 0;
-        for (final double dVal : sample) {
-            // scale to maximum amplitude
-            final short val = (short) ((dVal * 32767));
+        for (final short val : sample) {
             // in 16 bit wav PCM, first byte is the low order byte
             generatedSnd[idx++] = (byte) (val & 0x00ff);
             generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
